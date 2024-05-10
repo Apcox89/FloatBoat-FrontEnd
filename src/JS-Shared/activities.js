c6333e7f7db93cd929fd9c96d8bc3a0c1a7ece32
Andy Cox <apcox89@gmail.com>
2024-05-09 21:37:35 -0500

    now including src files

---
+++ src/JS-Shared/activities.js
@@ -0,0 +1,7 @@
+// Get the button element by its ID
+var button = document.getElementById('myButton');
+
+// Add a click event listener to the button
+button.addEventListener('click', function() {
+    window.open('https://getbootstrap.com/docs/5.3/getting-started/introduction/','_blank');
+});
